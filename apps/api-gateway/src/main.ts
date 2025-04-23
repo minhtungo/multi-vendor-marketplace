374b686aac22a645d276e81bac1a309038e36510
minhtungo <loducfc@gmail.com>
2025-04-22 20:17:06 -0400

    feat-api-gateway: set up cors, rate-limiters, core middlewares

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,12 +1,31 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
 import * as path from 'path';
-
+import cors from 'cors';
+import { env } from './config/env';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import { rateLimiter } from './middlewares/rate-limiter';
 const app = express();
+
+app.use(
+  cors({
+    origin: env.APP_ORIGIN,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
+
+// Middlewares
+app.use(morgan(env.NODE_ENV === 'development' ? 'dev' : 'combined'));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(cookieParser());
+
+// Rate limiting
+app.use(rateLimiter);
+
+// Set the application to trust the reverse proxy
+app.set('trust proxy', true);
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
