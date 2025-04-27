1cf291c361a48025be4baf8cfb8221eca9cd994a
minhtungo <loducfc@gmail.com>
2025-04-27 16:07:08 -0400

    refactor(auth-service): update entry point and remove main.ts file
    - Changed the main entry point from main.ts to index.ts in package.json.
    - Deleted the main.ts file, which contained the Express application setup and middleware configuration.

---
+++ apps/auth-service/src/index.ts
@@ -0,0 +1,23 @@
+import { env } from '@/configs/env';
+import { app } from '@/server';
+import { logger } from '@packages/utils/logger';
+import { createServer } from 'http';
+
+const httpServer = createServer(app);
+
+const server = httpServer.listen(env.PORT, () => {
+  const { NODE_ENV, HOST, PORT } = env;
+  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
+});
+
+const onCloseSignal = () => {
+  logger.info('sigint received, shutting down');
+  server.close(() => {
+    logger.info('server closed');
+    process.exit();
+  });
+  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+};
+
+process.on('SIGINT', onCloseSignal);
+process.on('SIGTERM', onCloseSignal);
--- apps/auth-service/src/server.ts
+++ apps/auth-service/src/server.ts
@@ -1,13 +1,14 @@
 import cors from 'cors';
-import express from 'express';
-import morgan from 'morgan';
+import express, { type Express } from 'express';
 import { env } from './configs/env';
 import { openAPIRouter } from './docs/openAPIRouter';
 import { rateLimiter } from './middlewares/rate-limiter';
 import cookieParser from 'cookie-parser';
 import { errorMiddleware } from '@packages/middlewares/error-handler';
+import { requestLogger } from '@packages/middlewares/error-logger';
+import { authRouter } from '@/modules/auth-router';
 
-const app = express();
+const app: Express = express();
 
 app.use(
   cors({
@@ -17,21 +18,23 @@ app.use(
   })
 );
 
-// Middlewares
-app.use(morgan(env.NODE_ENV === 'development' ? 'dev' : 'combined'));
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cookieParser());
-
-// Rate limiting
-app.use(rateLimiter);
-
 // Set the application to trust the reverse proxy
 app.set('trust proxy', true);
 
+// Middlewares
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(cookieParser());
+app.use(rateLimiter);
+
+//Request Logging
+app.use(requestLogger);
+
+//Routes
 app.get('/health', (req, res) => {
   res.send({ message: 'Welcome to auth service!' });
 });
+app.use('/api/auth', authRouter);
 
 // Swagger UI
 app.use(openAPIRouter);
@@ -39,9 +42,4 @@ app.use(openAPIRouter);
 // Error handling
 app.use(errorMiddleware);
 
-const port = env.PORT || 3001;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-
-server.on('error', console.error);
+export { app };
