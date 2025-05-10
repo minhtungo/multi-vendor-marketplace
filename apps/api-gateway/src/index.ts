dc5ba1b92f7fd0bf43b1471af81524139a56154f
minhtungo <loducfc@gmail.com>
2025-05-10 00:04:19 -0400

    chore(server): update TypeScript configuration and refactor package exports

--- apps/api-gateway/src/index.ts
+++ apps/api-gateway/src/index.ts
@@ -1,21 +1,21 @@
-import { env } from "@/configs/env";
-import { app } from "@/server";
-import { logger } from "@repo/server/lib/logger";
+import { env } from '@/configs/env';
+import { app } from '@/server';
+import { logger } from '@repo/server/lib/logger';
 
 const server = app.listen(env.PORT, () => {
-	const { NODE_ENV, HOST, PORT } = env;
-	logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
+  const { NODE_ENV, HOST, PORT } = env;
+  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
 });
 
 const onCloseSignal = async () => {
-	logger.info("sigint received, shutting down");
+  logger.info('sigint received, shutting down');
 
-	server.close(() => {
-		logger.info("server closed");
-		process.exit();
-	});
-	setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+  server.close(() => {
+    logger.info('server closed');
+    process.exit();
+  });
+  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
 };
 
-process.on("SIGINT", onCloseSignal);
-process.on("SIGTERM", onCloseSignal);
+process.on('SIGINT', onCloseSignal);
+process.on('SIGTERM', onCloseSignal);
